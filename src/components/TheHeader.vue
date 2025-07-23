255687079799c512716c7080434be4ac8176ca21
Guilherme Ferreti <guiferretis@gmail.com>
2025-07-23 19:45:49 -0300

    feat: add header callout component

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,5 +1,23 @@
+<script setup lang="ts">
+import TheHeaderCallout from './TheHeaderCallout.vue';
+</script>
+
 <template>
-  <header>
-    <h1>header</h1>
+  <header class="header | container">
+    <TheHeaderCallout />
   </header>
 </template>
+
+<style scoped lang="scss">
+.header {
+  padding-top: 1.75rem;
+
+  @include for-tablet-landscape-up {
+    padding-top: 1.25rem;
+  }
+
+  > *:not(:first-child) {
+    padding-top: 1rem;
+  }
+}
+</style>
---
+++ src/components/TheHeaderCallout.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+import ArrowRightIcon from '@/assets/icons/ArrowRight.svg';
+</script>
+
+<template>
+  <div class="header-callout | breakout">
+    <RouterLink
+      :to="{ name: 'home' }"
+      class="header-callout__link"
+    >
+      <span>Free Courses 🌟 Sale Ends Soon, Get It Now</span>
+      <ArrowRightIcon />
+    </RouterLink>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.header-callout {
+  border-radius: $radius--sm;
+  background-color: $color-primary;
+  padding: 0.625rem 1rem;
+  width: 100%;
+  color: $color-white;
+  font-size: $font-size--sm;
+
+  &__link {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 0.75rem;
+    text-align: center;
+
+    &:hover {
+      text-decoration: underline;
+    }
+
+    .icon {
+      flex-shrink: 0;
+    }
+  }
+}
+</style>
